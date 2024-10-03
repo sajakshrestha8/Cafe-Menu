7b43abc4e1fb2b69797f80b8bebfa430f659053c
sajakshrestha8 <shresthasajak123@gmail.com>
2024-10-03 22:18:56 +0545

    Updated index.js

--- Server/index.js
+++ Server/index.js
@@ -3,19 +3,42 @@ const app = express();
 const port = 8000;
 const swaggerDocs = require("./swagger");
 
+let momoItemStorage = [
+  {
+    name: "Fried Momo",
+    price: 170,
+  },
+  {
+    name: "Steam Momo",
+    price: 140,
+  },
+  {
+    name: "Jhol Momo",
+    price: 150,
+  },
+];
+
 swaggerDocs(app, port);
 
 /**
  * @swagger
  * /:
- * get:
- *  summary: Checking the Swagger UI documnetation
- *  description: checking hudai xa hai
- *  responses:
+ *  get:
+ *    summary: Checking the Swagger UI documnetation
+ *    description: checking hudai xa hai
+ *    responses:
  *      200:
  *          description: Check matrai gareko ho aaru kai ni haina
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  type: string
  */
-app.get("/", (req, res) => [res.send("Checking that is it working or not")]);
+app.get("/", (req, res) => {
+  res.send(momoItemStorage);
+});
 
 app.listen(port, () => {
   console.log("Server is running in port", port);
